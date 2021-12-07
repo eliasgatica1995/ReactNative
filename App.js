5da1b0ad6b478ec3757ca6c6387d66efa018fdb3
Elias Gatica <gonzaelias95@gmail.com>
2021-12-06 21:28:36 -0300

    Hola, Coder

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
+      <Text>Hola, Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
